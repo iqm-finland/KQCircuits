c74b1b005749b2a5dc0bc205afc4ceaeee099675
Jukka Räbinä <jukka@meetiqm.com>
2021-04-07 15:29:53 +0300

    Generalize QualityFactorTwoface to cover variable resonator widths and etched top chip
    The changes are needed for studying resonator properties. Changes in more details:
    
    - Enable independent trace and gap widths for each resonators (parameters `res_a` and `res_b`).
    - Add option "Etched top chip (4)" to 'resonator_type'. In this option, all waveguides and capacitors have etched area on top chip above the protection layer.
    - Move some hard-coded parameters to element parameters. This includes `x_indentation`, `cap_res_distance`, and `waveguide_indentation`.
    - Enable negative 'waveguide_indentation' so that waveguide bends are outside the top chip area.
    - Refactor class QualityFactorTwoface to improve code structure.

--- kqcircuits/chips/multi_face/quality_factor_twoface.py
+++ kqcircuits/chips/multi_face/quality_factor_twoface.py
@@ -30,23 +30,28 @@ class QualityFactorTwoface(MultiFace):
      """
 
     res_lengths = Param(pdt.TypeList, "Resonator lengths", [5434, 5429, 5374, 5412, 5493, 5589], unit="[μm]",
-        docstring="Physical length of resonators [μm]")
+                        docstring="Physical length of resonators [μm]")
     n_fingers = Param(pdt.TypeList, "Number of fingers of the coupler", [4, 4, 2, 4, 4, 4],
-        docstring="Fingers in planar capacitors")
-    l_fingers = Param(pdt.TypeList, "Length of fingers", [23.1, 9.9, 14.1, 10, 21, 28, 3], unit="[μm]",
-        docstring="Length of the capacitor fingers [μm]")
-    type_coupler = Param(pdt.TypeList, "Coupler type", ["interdigital", "interdigital", "interdigital", "gap", "gap", "gap"])
-    res_a = Param(pdt.TypeDouble, "Resonator waveguide center conductor width", 10, unit="[μm]",
-        docstring="Width of the center conductor in the resonator [μm]")
-    res_b = Param(pdt.TypeDouble, "Resonator waveguide gap width", 10, unit="[μm]",
-        docstring="Width of the gap in the resonator [μm]")
+                      docstring="Fingers in planar capacitors")
+    l_fingers = Param(pdt.TypeList, "Length of fingers", [23.1, 9.9, 14.1, 10, 21, 28], unit="[μm]",
+                      docstring="Length of the capacitor fingers [μm]")
+    type_coupler = Param(pdt.TypeList, "Coupler type",
+                         ["interdigital", "interdigital", "interdigital", "gap", "gap", "gap"])
+    res_a = Param(pdt.TypeList, "Resonator waveguide center conductor width", [10, 10, 10, 20, 10, 5], unit="[μm]",
+                  docstring="Width of the center conductor in the resonators [μm]")
+    res_b = Param(pdt.TypeList, "Resonator waveguide gap width", [6, 6, 6, 12, 6, 3], unit="[μm]",
+                  docstring="Width of the gap in the resonators [μm]")
     resonator_type = Param(pdt.TypeString, "Routing type", "capped",
-        choices=[["Capped (1)", "capped"], ["Two-face resonator (2)", "twoface"], ["Resonator on top (3)", "top"]])
+                           choices=[["Capped (1)", "capped"], ["Two-face resonator (2)", "twoface"],
+                                    ["Resonator on top (3)", "top"], ["Etched top chip (4)", "etched"]])
     connector_distances = Param(pdt.TypeList, "Resonator input to face to face connector",
-        [500, 1300, 2100, 2900, 3700, 4500], unit="[μm]",
-        docstring="Distances of face to face connectors from resonator inputs")
+                                [500, 1300, 2100, 2900, 3700, 4500], unit="[μm]",
+                                docstring="Distances of face to face connectors from resonator inputs")
     spiral_box_height = Param(pdt.TypeDouble, "Spiral resonator box height", 2000)
     spiral_box_width = Param(pdt.TypeDouble, "Spiral resonator box width", 500)
+    x_indentation = Param(pdt.TypeDouble, "Resonator/connector indentation from side edges", 800)
+    cap_res_distance = Param(pdt.TypeDouble, "Distance between spiral resonator and capacitor", 200)
+    waveguide_indentation = Param(pdt.TypeDouble, "Waveguide indentation from top chip edge", 500)
 
     def produce_impl(self):
         self._produce_resonators()
@@ -55,78 +60,44 @@ class QualityFactorTwoface(MultiFace):
         super().produce_impl()
 
     def _produce_resonators(self):
-        # Magic parameters
-
-        cap_indentation = 200  # indentation counted from finger capacitor,
-        # so the spiral resonator distance from TL is controllable
-        tl_connector_indent = 525
-
-        face_config = ["t", "b"] if self.resonator_type == "top" else ["b", "t"]
-
         # Interpretation of parameter lists
         res_lengths = [float(foo) for foo in self.res_lengths]
+        res_a = [float(foo) for foo in self.res_a]
+        res_b = [float(foo) for foo in self.res_b]
         n_fingers = [int(foo) for foo in self.n_fingers]
         type_coupler = self.type_coupler
         l_fingers = [float(foo) for foo in self.l_fingers]
         connector_distances = [float(foo) for foo in self.connector_distances]
 
-        # Launchers
-        launchers = self.produce_launchers_SMA8(enabled=["WN", "EN"])
+        # Constants
+        left_x = self.face1_box.p1.x + self.waveguide_indentation
+        right_x = self.face1_box.p2.x - self.waveguide_indentation
+        left_connector = self.face1_box.p1.x + self.x_indentation
+        right_connector = self.face1_box.p2.x - self.x_indentation
+        mid_y = (self.face1_box.p1.y + self.face1_box.p2.y) / 2
+        face_config = ["t", "b"] if self.resonator_type == "top" else ["b", "t"]
 
-        # Taper locations
-        taper_pos_left = pya.DPoint(self.face1_box.p1.x,
-                                    self.refpoints["WN_port"].y)  # add taper to produce impedance match
-        taper_pos_right = pya.DPoint(self.face1_box.p2.x - 100,
-                                     self.refpoints["EN_port"].y)  # add taper to produce impedance match
-
-        marker_safety = 1.5e3  # depends on the marker size
-
-        if self.resonator_type == "top":
-            connector_node = Node((2000 + tl_connector_indent, 5000), face_id="t")
-        else:
-            connector_node = Node((2000 + tl_connector_indent, 5000))
-
-        final_point = pya.DPoint(2000 + tl_connector_indent + 100, 5000)
-        nodes = [Node(self.refpoints["WN_port"]),
-                 Node(taper_pos_left, a=self.a_capped, b=self.b_capped),
-                 Node((2000, self.refpoints["WN_port"].y)),
-                 Node((2000, 5000)),
-                 connector_node,
-                 Node(final_point)
-                 ]
-
-        self.insert_cell(WaveguideComposite,
-                         nodes=nodes,
-                         margin=self.margin,
-                         a=self.a,
-                         b=self.b
-                         )
-
-        points_fl = [final_point]
-        tl_start = points_fl[-1]
-
+        # Create resonators
         resonators = len(self.res_lengths)
-        v_res_step = (pya.DPoint(self.refpoints["EN_port"].x, 5000) -
-                      pya.DPoint(self.refpoints["WN_port"].x, 5000) -
-                      pya.DVector((self.r * 4 + marker_safety * 2), 0)
-                      ) * (1. / resonators)
-        cell_cross = self.add_element(WaveguideCoplanarTCross,
-                                   length_extra_side=5 * self.a, a=self.a_capped, b=self.b_capped, a2=self.a_capped,
-                                   b2=self.b_capped,
-                                   face_ids=face_config)
+        tl_start = pya.DPoint(left_connector + self.spiral_box_width, mid_y)
+        v_res_step = pya.DPoint(right_connector - left_connector - self.spiral_box_width, 0) * \
+                     (1. / resonators)
+        cell_cross = self.add_element(WaveguideCoplanarTCross, length_extra_side=5 * self.a_capped,
+                                      a=self.a_capped, b=self.b_capped, a2=self.a_capped, b2=self.b_capped,
+                                      face_ids=face_config, margin=self.margin)
 
         for i in range(resonators):
             cplr = produce_library_capacitor(self.layout, n_fingers[i], l_fingers[i], type_coupler[i],
-                                             face_ids=face_config)
+                                             face_ids=face_config, margin=self.margin,
+                                             a=res_a[i], b=res_b[i], a2=self.a_capped, b2=self.b_capped)
             cplr_refpoints_rel = self.get_refpoints(cplr)
 
-            # rotate if bool(i%2)
-            if bool(i % 2):
+            # Every second resonator is on the same side. Define transformations here:
+            if i % 2:
                 rotation = pya.DTrans.R90 * pya.DTrans.M0
                 rot_2 = pya.DTrans.M0
                 rot_3 = pya.DTrans.R270
                 cplr_pos_post = pya.DTrans.R90 * rot_2 * cplr_refpoints_rel["port_b"]
-
             else:
                 rotation = pya.DTrans.R270
                 rot_2 = pya.DTrans.R0
@@ -139,75 +110,108 @@ class QualityFactorTwoface(MultiFace):
 
             # Coupler
             cplr_pos = cross_refpoints_abs["port_bottom"] + cplr_pos_post
-            cplr_trans = pya.DTrans(1, False, cplr_pos.x, cplr_pos.y)
+            cplr_trans = pya.DTrans(cplr_pos.x, cplr_pos.y) * rot_3
             self.insert_cell(cplr, cplr_trans)
 
-            # indentation for spiral resonator
+            # Y-indentation for spiral resonator
             endpoint = point_shift_along_vector(cplr_refpoints_rel["port_b"],
                                                 cplr_refpoints_rel["port_b_corner"],
-                                                cap_indentation)
+                                                self.cap_res_distance)
 
             self.insert_cell(WaveguideCoplanar, **{**self.cell.pcell_parameters_by_name(), **{
                 "path": pya.DPath([cplr_refpoints_rel["port_b"], endpoint], 1),
                 "term2": 0,
-                "a": self.a_capped,
-                "b": self.b_capped,
+                "a": res_a[i],
+                "b": res_b[i],
+                "r": self.r,
                 "face_ids": face_config,
                 "margin": self.margin}},
-                trans=pya.DTrans(cplr_pos) * rotation)
+                             trans=pya.DTrans(cplr_pos) * rotation)
 
             pos_res_start = cplr_pos - rot_3 * endpoint
 
+            # Spiral resonator
             if self.resonator_type == "twoface":
                 cell_res_even_width = self.add_element(SpiralResonatorMultiface,
-                                                    right_space=self.spiral_box_height - cap_indentation,
-                                                    above_space=0,
-                                                    below_space=self.spiral_box_width,
-                                                    length=res_lengths[i] - cap_indentation,
-                                                    a=self.res_a,
-                                                    b=self.res_b,
-                                                    margin=self.margin,
-                                                    connector_dist=connector_distances[i] - cap_indentation,
-                                                    face_ids=face_config
-                                                    )
-
+                                                       right_space=self.spiral_box_height - self.cap_res_distance,
+                                                       above_space=0,
+                                                       below_space=self.spiral_box_width,
+                                                       length=res_lengths[i] - self.cap_res_distance,
+                                                       a=res_a[i],
+                                                       b=res_b[i],
+                                                       margin=self.margin,
+                                                       connector_dist=connector_distances[i] - self.cap_res_distance,
+                                                       face_ids=face_config,
+                                                       r=self.r
+                                                       )
             else:
                 cell_res_even_width = self.add_element(SpiralResonator,
-                                                    right_space=self.spiral_box_height - cap_indentation,
-                                                    above_space=0,
-                                                    below_space=self.spiral_box_width,
-                                                    length=res_lengths[i] - cap_indentation,
-                                                    a=self.res_a,
-                                                    b=self.res_b,
-                                                    margin=self.margin,
-                                                    face_ids=face_config
-                                                    )
-
+                                                       name='resonator{}'.format(i),
+                                                       right_space=self.spiral_box_height - self.cap_res_distance,
+                                                       above_space=0,
+                                                       below_space=self.spiral_box_width,
+                                                       length=res_lengths[i] - self.cap_res_distance,
+                                                       a=res_a[i],
+                                                       b=res_b[i],
+                                                       margin=self.margin,
+                                                       face_ids=face_config,
+                                                       r=self.r
+                                                       )
             self.insert_cell(cell_res_even_width, pya.DTrans(pos_res_start) * rotation)
+
             # Feedline
-            self.insert_cell(WaveguideCoplanar, **{**self.cell.pcell_parameters_by_name(), **{
-                "path": pya.DPath(points_fl + [cross_refpoints_abs["port_left"]], 1),
-                "term2": 0,
-                "a": self.a_capped,
-                "b": self.b_capped,
-                "margin": self.margin,
-                "face_ids": face_config
-            }})
-            points_fl = [cross_refpoints_abs["port_right"]]
+            if i == 0:
+                left_point = cross_refpoints_abs["port_left"]
+            else:
+                self.insert_cell(WaveguideCoplanar, **{**self.cell.pcell_parameters_by_name(), **{
+                    "path": pya.DPath([right_point] + [cross_refpoints_abs["port_left"]], 1),
+                    "term2": 0,
+                    "a": self.a_capped,
+                    "b": self.b_capped,
+                    "r": self.r,
+                    "margin": self.margin,
+                    "face_ids": face_config
+                }})
+            right_point = cross_refpoints_abs["port_right"]
 
-        if self.resonator_type == "top":
-            connector_node = Node((8000 - tl_connector_indent + 103, 5000), face_id="b")
+        # Launchers
+        self.produce_launchers_SMA8(enabled=["WN", "EN"])
+
+        # Waveguides to the launchers
+        if self.waveguide_indentation > 0.0:
+            nodes_left = [Node(self.refpoints["WN_port"]),
+                          Node((self.face1_box.p1.x, self.refpoints["WN_port"].y), a=self.a_capped, b=self.b_capped),
+                          Node((left_x, self.refpoints["WN_port"].y)),
+                          Node((left_x, mid_y)),
+                          Node((left_connector, mid_y), face_id=face_config[0]),
+                          Node(left_point)]
+            nodes_right = [Node(self.refpoints["EN_port"]),
+                           Node((self.face1_box.p2.x, self.refpoints["EN_port"].y), a=self.a_capped, b=self.b_capped),
+                           Node((right_x, self.refpoints["EN_port"].y)),
+                           Node((right_x, mid_y)),
+                           Node((right_connector, mid_y), face_id=face_config[0]),
+                           Node(right_point)]
         else:
-            connector_node = Node((8000 - tl_connector_indent + 50, 5000))
+            nodes_left = [Node(self.refpoints["WN_port"]),
+                          Node((left_x, self.refpoints["WN_port"].y)),
+                          Node((left_x, mid_y)),
+                          Node((self.face1_box.p1.x, mid_y), a=self.a_capped, b=self.b_capped),
+                          Node((left_connector, mid_y), face_id=face_config[0]),
+                          Node(left_point)]
+            nodes_right = [Node(self.refpoints["EN_port"]),
+                           Node((right_x, self.refpoints["EN_port"].y)),
+                           Node((right_x, mid_y)),
+                           Node((self.face1_box.p2.x, mid_y), a=self.a_capped, b=self.b_capped),
+                           Node((right_connector, mid_y), face_id=face_config[0]),
+                           Node(right_point)]
+        self.insert_cell(WaveguideComposite, nodes=nodes_left, margin=self.margin, a=self.a, b=self.b)
+        self.insert_cell(WaveguideComposite, nodes=nodes_right, margin=self.margin, a=self.a, b=self.b)
 
-        # Last feedline
-        nodes = [Node(points_fl[-1]),
-                 connector_node,
-                 Node((8000, 5000)),
-                 Node((8000, self.refpoints["EN_port"].y)),
-                 Node(taper_pos_right, a=self.a, b=self.b),
-                 Node(self.refpoints["EN_port"])
-                 ]
-
-        self.insert_cell(WaveguideComposite, nodes=nodes, margin=self.margin, a=self.a_capped, b=self.b_capped,
-                         face_ids=face_config)
+        # Etched top chip at protection layer
+        if self.resonator_type == "etched":
+            region = pya.Region(self.cell.begin_shapes_rec(self.get_layer("ground_grid_avoidance", face_id=0)))
+            region &= pya.Region([pya.DPolygon([
+                self.face1_box.p1, pya.DPoint(self.face1_box.p1.x, self.face1_box.p2.y),
+                self.face1_box.p2, pya.DPoint(self.face1_box.p2.x, self.face1_box.p1.y)
+            ]).to_itype(self.layout.dbu)])
+            self.cell.shapes(self.get_layer("base_metal_gap_wo_grid", face_id=1)).insert(region)
