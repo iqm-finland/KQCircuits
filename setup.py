060bf18d12bc38c55048bbfdcac383826e1d2c12
Caspar <caspar@meetiqm.com>
2021-05-25 19:34:29 +0300

    Faster and more flexible ground bump grid in MultiFace

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kqcircuits',
-    version="2.3.2",
+    version="2.4.0",
     description="KQCircuits is a KLayout/Python-based superconducting quantum circuit library developed by IQM.",
     author="IQM Finland Oy",
     author_email="developers@meetiqm.com",
