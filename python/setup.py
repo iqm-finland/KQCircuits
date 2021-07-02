c917c189671be726c86fda75414210f4a464c140
Janne Kotilahti <janne@meetiqm.com>
2021-07-02 19:51:48 +0300

    Use pylint for static code analysis in CI

--- python/setup.py
+++ python/setup.py
@@ -48,7 +48,7 @@ setup(
     ],
     extras_require={
         "docs": ["sphinx~=2.4", "sphinx-rtd-theme~=0.4"],
-        "tests": ["pytest>=6.0.2", "pytest-cov~=2.8", "pytest-xdist>=2.1", "tox>=3.18"],
+        "tests": ["pytest>=6.0.2", "pytest-cov~=2.8", "pytest-xdist>=2.1", "tox>=3.18", "pylint>=2.9"],
         "gds_export": ["gdspy~=1.5"],
         "png_export": ["cairosvg~=2.4"],
     },
