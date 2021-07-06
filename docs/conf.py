b7afee36cecdba6e365e1ab5c544f8701c8baf2f
Niko Savola <niko@meetiqm.com>
2021-07-06 05:39:30 +0000

    Add favicon to documentation and make logo.svg dark-mode aware

--- docs/conf.py
+++ docs/conf.py
@@ -157,6 +157,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'api/modules.rst']
 html_theme = 'sphinx_rtd_theme'
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
+# Add favicon to site. Vectors are supported by modern browsers.
+html_favicon = 'images/logo.svg'
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
