e421a088f36df52bd2c70eb92a55e9ea85374ccb
Niko Savola <niko@meetiqm.com>
2021-07-30 05:36:42 +0000

    Add Circular Capacitor simulations similar to FingerCapacitorSim

---
+++ klayout_package/python/kqcircuits/simulations/circular_capacitor_sim.py
@@ -0,0 +1,34 @@
+# This code is part of KQCircuits
+# Copyright (C) 2021 IQM Finland Oy
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see
+# https://www.gnu.org/licenses/gpl-3.0.html.
+#
+# The software distribution should follow IQM trademark policy for open-source software
+# (meetiqm.com/developers/osstmpolicy). IQM welcomes contributions to the code. Please see our contribution agreements
+# for individuals (meetiqm.com/developers/clas/individual) and organizations (meetiqm.com/developers/clas/organization).
+
+from kqcircuits.elements.circular_capacitor import CircularCapacitor
+from kqcircuits.pya_resolver import pya
+from kqcircuits.simulations.simulation import Simulation
+from kqcircuits.util.parameters import add_parameters_from
+
+
+@add_parameters_from(CircularCapacitor)
+class CircularCapacitorSim(Simulation):
+
+    def build(self):
+        capacitor_cell = self.add_element(CircularCapacitor, **self.get_parameters())
+
+        cap_trans = pya.DTrans(0, False, (self.box.left + self.box.right) / 2, (self.box.bottom + self.box.top) / 2)
+        _, refp = self.insert_cell(capacitor_cell, cap_trans)
+
+        self.produce_waveguide_to_port(refp["port_a"], refp["port_a_corner"], 1, 'left')
+        self.produce_waveguide_to_port(refp["port_b"], refp["port_b_corner"], 2, 'right')
---
+++ klayout_package/python/scripts/simulations/circular_capacitor_library_sim.py
@@ -0,0 +1,125 @@
+# This code is part of KQCircuits
+# Copyright (C) 2021 IQM Finland Oy
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see
+# https://www.gnu.org/licenses/gpl-3.0.html.
+#
+# The software distribution should follow IQM trademark policy for open-source software
+# (meetiqm.com/developers/osstmpolicy). IQM welcomes contributions to the code. Please see our contribution agreements
+# for individuals (meetiqm.com/developers/clas/individual) and organizations (meetiqm.com/developers/clas/organization).
+
+import logging
+import subprocess
+import sys
+from pathlib import Path
+
+from kqcircuits.pya_resolver import pya
+from kqcircuits.simulations.circular_capacitor_sim import CircularCapacitorSim
+from kqcircuits.simulations.export.ansys.ansys_export import export_ansys
+from kqcircuits.simulations.export.simulation_export import cross_sweep_simulation, export_simulation_oas
+from kqcircuits.util.export_helper import create_or_empty_tmp_directory, get_active_or_new_layout
+
+# Prepare output directory
+dir_path = create_or_empty_tmp_directory(Path(__file__).stem + "_output")
+
+sim_class = CircularCapacitorSim  # pylint: disable=invalid-name
+
+# Simulation parameters
+sim_parameters = {
+    'name': 'circular_capacitor',
+    'use_internal_ports': True,
+    'use_ports': True,
+    'box': pya.DBox(pya.DPoint(0, 0), pya.DPoint(1000, 1000)),
+    'port_width': 200,
+    'wafer_stack_type': 'planar',
+    'corner_r': 2,
+    'chip_distance': 8,
+
+    'ground_gap': 20,
+    'fixed_length': 0,
+    'r_inner': 75,
+    'r_outer': 120,
+    'swept_angle': 180,
+    'outer_island_width': 40,
+    'a': 10,
+    'b': 6,
+}
+export_parameters = {
+    'path': dir_path,
+    'ansys_tool': 'q3d',
+    'exit_after_run': True,
+    'percent_error': 0.3,
+    'minimum_converged_passes': 2,
+    'maximum_passes': 20,
+}
+
+# Sweep ranges
+chip_distances = [4, 5, 6, 7, 8, 9, 10, 12, 14, 18, 22]
+r_inner = [10, 75]
+swept_angle = [10, 40, 70, 100, 130, 160, 180, 190, 220, 250, 280, 310, 340]
+ab_single = [(10, 6), (5, 20), (4, 30)]
+ab_multi = [(4.0, 2.5), (3.5, 32.0), (2.0, 32.0)]
+
+# Get layout
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+layout = get_active_or_new_layout()
+
+# Cross sweep number of fingers and finger length
+simulations = []
+
+# Smaller geometry single face sweep
+for ab in ab_single:
+    simulations += cross_sweep_simulation(layout, sim_class, sim_parameters, {
+        'r_inner': [10],
+        'r_outer': [55],
+        'outer_island_width': [20],
+        'swept_angle': swept_angle,
+        'a': [ab[0]],
+        'a2': [ab[0]],
+        'b': [ab[1]],
+        'b2': [ab[1]],
+    })
+
+# Single face sweep
+for ab in ab_single:
+    simulations += cross_sweep_simulation(layout, sim_class, sim_parameters, {
+        'r_inner': r_inner,
+        'swept_angle': swept_angle,
+        'a': [ab[0]],
+        'a2': [ab[0]],
+        'b': [ab[1]],
+        'b2': [ab[1]],
+    })
+
+for ab in ab_multi:
+    simulations += cross_sweep_simulation(layout, sim_class,
+        sim_parameters | {'wafer_stack_type': "multiface"}, {
+        'chip_distance': chip_distances,
+        'r_inner': r_inner,
+        'swept_angle': swept_angle,
+        'a': [ab[0]],
+        'a2': [ab[0]],
+        'b': [ab[1]],
+        'b2': [ab[1]],
+    })
+
+
+# simulations for getting the ballpark
+# simulations += cross_sweep_simulation(layout, sim_class, sim_parameters, {
+#     'r_inner': np.arange(10, 40+31, 5, dtype=int).tolist(),
+#     'r_outer': np.arange(100-10, 100+11, 5, dtype=int).tolist(),
+#     'swept_angle': [10, 180, 340],
+# })
+
+# Export Ansys files
+export_ansys(simulations, **export_parameters)
+
+# Write and open oas file
+subprocess.call(export_simulation_oas(simulations, dir_path), shell=True)
