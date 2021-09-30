ef272304ebcf1b879ef9a1cfdb3a1cf59ccf0b56
Niko Savola <niko@meetiqm.com>
2021-09-30 08:46:21 +0000

    Run start-up macros in Docker image for CLI usage
    * Add note about Docker memory limit with HyperV backend on Windows

--- ci/run_script.sh
+++ ci/run_script.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 Xvfb :99 -screen 0 640x480x24 &
-if klayout -e -z -nc -rx -r "$@" ; then
+if klayout -e -z -nc -r "$@" ; then
   echo "Succeeded in running KQCircuits."
 else
   echo "KQCircuits failed to run. See https://iqm-finland.github.io/KQCircuits/developer/docker_image.html for instructions".
