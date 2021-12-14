7d22d1b5bbe63f63f427cdfa55362d3506313c86
Niko Savola <niko@meetiqm.com>
2021-12-14 14:18:59 +0000

    Check confidential content in git history

--- ci/check_files_for_strings.sh
+++ ci/check_files_for_strings.sh
@@ -6,9 +6,11 @@
 # There should be one string per line in the input file.
 # Returns 0 if no instances of the strings are found, returns 1 otherwise.
 #
-# usage: ci/check_files_for_strings.sh strings_to_find.txt
+# usage: ci/check_files_for_strings.sh strings_to_find.txt master 123-feature-branch
 
 file="$1"
+target_branch="$2"
+source_branch="$3"
 
 excluded_paths=( \
 "*.pip-cache*" \
@@ -18,6 +20,7 @@ excluded_paths=( \
 "*tmp" \
 "*pycache*" \
 "./grain.xml" \
+"*/.tox*"
 )
 for path in "${excluded_paths[@]}"; do
   # arguments for "find" to exclude paths
@@ -29,15 +32,40 @@ echo "Checking files for strings given in $file..."
 found_string=false
 
 echo "Filenames or folder names containing given strings:"
-if find . | grep -if "$file"; then
+if find . -path "*refs/remotes*" -prune -o -print | grep -if "$file"; then
   found_string=true
 fi
 
 echo "Files containing given strings:"
-if find . "${path_arguments[@]}" -type f -exec grep -ilf "$file" {} +;
-then
+if find . "${path_arguments[@]}" -type f -exec grep -ilf "$file" {} +; then
   found_string=true
 fi
+
+echo "Git diff or commit message history containing given strings:"
+while IFS="" read -r p || [ -n "$p" ]; do
+
+  # Check git diffs
+  log_patch=$(git log -i --oneline -G "$p" "$target_branch..$source_branch")
+  if [[ -n $log_patch ]]; then
+    found_string=true
+    echo "$p" "$log_patch"
+  fi
+
+  # Check commit messages
+  log_grep=$(git log -i --oneline --grep "$p" "$target_branch..$source_branch")
+  if [[ -n $log_grep ]]; then
+    found_string=true
+    echo "$p" "$log_grep"
+  fi
+
+  # Check names of files that might have been removed
+  log_files=$(git log --oneline --name-only "$target_branch..$source_branch")
+  if [[ $log_files =~ $p ]]; then
+    found_string=true
+    echo "$p" "$log_files"
+  fi
+
+done < "$file"
 
 if [ "$found_string" = true ]; then
   echo "Found instances of given strings."
--- ci/init_kqc.sh
+++ ci/init_kqc.sh
@@ -1,3 +1,4 @@
-mkdir -p $HOME/.klayout/python .pip-cache
+#!/bin/sh
+mkdir -p "$HOME"/.klayout/python .pip-cache
 pip --cache-dir=.pip-cache install -e "klayout_package/python[docs,tests]"
 python setup_within_klayout.py
