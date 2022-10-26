f5b044074829b17b11069d94831458b9d0f3036e
Eelis Takala <eelis@meetiqm.com>
2022-10-26 14:52:22 +0300

    Fix `InternalPort` w/o `ground_location`

--- klayout_package/python/scripts/simulations/elmer/scripts/gmsh_helpers.py
+++ klayout_package/python/scripts/simulations/elmer/scripts/gmsh_helpers.py
@@ -886,23 +886,24 @@ def export_gmsh_msh(sim_data: dict, path: Path, default_mesh_size: float = 100, 
             body_port_phys_map[dim_tag] = []
         for port in port_data_gmsh:
             port['physical_names'] = []
-            if port['type'] == 'InternalPort':
-                port_physical_name = 'port_' + str(port['number'])
-                gmsh.model.setPhysicalName(*port['dim_tag'], port_physical_name)
-                parent_body_dim_tag = get_parent_body(port['dim_tag'], body_dim_tags)
-                if parent_body_dim_tag is not None:
-                    port['physical_names'].append((parent_body_dim_tag, port_physical_name))
-                    body_port_phys_map[parent_body_dim_tag].append(port_physical_name)
-
-            else:
-                port['dim_tags'] = get_entities_in_bounding_boxes([port['occ_bounding_box']], 2)
-                for i, dim_tag in enumerate(port['dim_tags']):
-                    port_physical_name = 'port_{}_{}'.format(port['number'], i)
-                    gmsh.model.setPhysicalName(*dim_tag, port_physical_name)
-                    parent_body_dim_tag = get_parent_body(dim_tag, body_dim_tags)
+            if 'polygon' in port:
+                if port['type'] == 'InternalPort':
+                    port_physical_name = 'port_' + str(port['number'])
+                    gmsh.model.setPhysicalName(*port['dim_tag'], port_physical_name)
+                    parent_body_dim_tag = get_parent_body(port['dim_tag'], body_dim_tags)
                     if parent_body_dim_tag is not None:
                         port['physical_names'].append((parent_body_dim_tag, port_physical_name))
                         body_port_phys_map[parent_body_dim_tag].append(port_physical_name)
+
+                else:
+                    port['dim_tags'] = get_entities_in_bounding_boxes([port['occ_bounding_box']], 2)
+                    for i, dim_tag in enumerate(port['dim_tags']):
+                        port_physical_name = 'port_{}_{}'.format(port['number'], i)
+                        gmsh.model.setPhysicalName(*dim_tag, port_physical_name)
+                        parent_body_dim_tag = get_parent_body(dim_tag, body_dim_tags)
+                        if parent_body_dim_tag is not None:
+                            port['physical_names'].append((parent_body_dim_tag, port_physical_name))
+                            body_port_phys_map[parent_body_dim_tag].append(port_physical_name)
 
         if port['type'] == 'EdgePort':
             port['dim_tags'] = get_entities_in_bounding_boxes([port['occ_bounding_box']], 2)
