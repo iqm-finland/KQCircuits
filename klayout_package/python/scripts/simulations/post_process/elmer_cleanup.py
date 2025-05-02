cc8f18eb47219445c97d3b8bc0527b3befca3406
Tuomas Myllari <tuomas.myllari@meetiqm.com>
2025-05-02 18:27:11 +0300

    Add post-processing script `elmer_cleanup.py` for deleting Elmer and Gmsh mesh files

---
+++ klayout_package/python/scripts/simulations/post_process/elmer_cleanup.py
@@ -0,0 +1,40 @@
+# This code is part of KQCircuits
+# Copyright (C) 2025 IQM Finland Oy
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see
+# https://www.gnu.org/licenses/gpl-3.0.html.
+#
+# The software distribution should follow IQM trademark policy for open-source software
+# (meetiqm.com/iqm-open-source-trademark-policy). IQM welcomes contributions to the code.
+# Please see our contribution agreements for individuals (meetiqm.com/iqm-individual-contributor-license-agreement)
+# and organizations (meetiqm.com/iqm-organization-contributor-license-agreement).
+
+"""
+Deletes all Elmer and Gmsh mesh files from the current tmp folder
+"""
+
+from pathlib import Path
+import shutil
+
+path = Path.cwd()
+
+for msh_file in path.glob("*.msh"):
+    msh_file.unlink()
+
+elmer_mesh_files = ["mesh.nodes", "mesh.elements", "mesh.boundary"]
+non_sim_dirs = ["scripts", "log_files", "elmer_data", "s_matrix_plots"]
+for p in path.iterdir():
+    if p.is_dir() and p not in non_sim_dirs:
+        for ef in elmer_mesh_files:
+            (p / ef).unlink(missing_ok=True)
+
+        for part_folder in p.glob("partitioning.*"):
+            if part_folder.is_dir():
+                shutil.rmtree(part_folder)
