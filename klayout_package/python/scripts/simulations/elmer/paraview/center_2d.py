538256a85c5f10cc7599d46b0139705b63730991
Inderpal Suthar <149688882+InderpalSuthar@users.noreply.github.com>
2025-06-09 16:19:36 +0530

    Update center_2d.py

--- klayout_package/python/scripts/simulations/elmer/paraview/center_2d.py
+++ klayout_package/python/scripts/simulations/elmer/paraview/center_2d.py
@@ -1,9 +1,7 @@
 import os
 from paraview.simple import *
 
-# Disable auto camera reset on first render
 paraview.simple._DisableFirstRenderCameraReset()
-
 
 def run(vtu_files: list, sif_path, is_3d=False):
     # Check for VTU file existence
@@ -11,36 +9,42 @@ def run(vtu_files: list, sif_path, is_3d=False):
         if not os.path.isfile(file):
             raise FileNotFoundError(f"VTU file not found: {file}")
 
-    print(f"[INFO] Loading VTU files: {vtu_files}")
-
-    # Load VTU files
     reader = XMLUnstructuredGridReader(FileName=vtu_files)
     reader.TimeArray = 'None'
 
-    # Update animation scene based on data time steps
     animationScene1 = GetAnimationScene()
     animationScene1.UpdateAnimationUsingDataTimeSteps()
 
-    # Get or create render view
     renderView = GetActiveViewOrCreate('RenderView')
     renderView.ResetCamera(False, 0.9)
 
-    # Show the data in the render view
     display = Show(reader, renderView, 'UnstructuredGridRepresentation')
     display.Representation = 'Surface'
 
-    # Color by 'potential'
     ColorBy(display, ('POINTS', 'potential'))
     display.RescaleTransferFunctionToDataRange(True, False)
     display.SetScalarBarVisibility(renderView, True)
 
-    # Set fixed camera parameters
     renderView.InteractionMode = '2D'
-    renderView.CameraPosition = [3.4454646530245864e-05, -2.9219767016599924e-07, 0.0029971506617571374]
-    renderView.CameraFocalPoint = [3.4454646530245864e-05, -2.9219767016599924e-07, 0.0]
-    renderView.CameraViewUp = [0, 1, 0]
-    renderView.CameraParallelScale = 6.508711041901536e-05
 
-    # Render and enable interaction
+    bounds = reader.GetDataInformation().GetBounds()
+    x_min, x_max, y_min, y_max, z_min, z_max = bounds
+
+    center_x = (x_min + x_max) / 2
+    center_y = (y_min + y_max) / 2
+    center_z = (z_min + z_max) / 2
+
+    half_height = (y_max - y_min) / 2
+    half_width = (x_max - x_min) / 2
+
+    parallel_scale = max(half_height, half_width) * 0.5
+
+    camera_position = [center_x, center_y, center_z + 1]  
+
+    renderView.CameraPosition = camera_position
+    renderView.CameraFocalPoint = [center_x, center_y, center_z]
+    renderView.CameraViewUp = [0, 1, 0]
+    renderView.CameraParallelScale = parallel_scale
+
     Render()
     Interact()
