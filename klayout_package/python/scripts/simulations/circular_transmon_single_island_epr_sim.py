1d7b5aafe0ce956b5e8c1cc0352d8260ab855853
Clinch Leatherwood <75779966+AbdullahKazi500@users.noreply.github.com>
2025-06-11 20:32:42 +0530

    Update klayout_package/python/scripts/simulations/circular_transmon_single_island_epr_sim.py
    Co-authored-by: Pavel Smirnov <109152229+qpavsmi@users.noreply.github.com>

--- klayout_package/python/scripts/simulations/circular_transmon_single_island_epr_sim.py
+++ klayout_package/python/scripts/simulations/circular_transmon_single_island_epr_sim.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python3
 # This code is part of KQCircuits
-# Copyright (C) 2024 IQM Finland Oy
+# Copyright (C) 2025 IQM Finland Oy
 #
 # This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
 # License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later
