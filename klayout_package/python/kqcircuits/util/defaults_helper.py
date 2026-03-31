22b0255fad493d907be4b1140559a4bbd1657643
Tuomas Myllari <tuomas.myllari@meetiqm.com>
2026-03-31 11:25:40 +0300

    Add latest Ansys installation path on Windows

--- klayout_package/python/kqcircuits/util/defaults_helper.py
+++ klayout_package/python/kqcircuits/util/defaults_helper.py
@@ -24,6 +24,7 @@ def find_ansys_executable(default):
     """Finds latest Ansys Electronics executable from default installation locations. Returns 'default' if not found."""
     paths = [
         (Path(os.environ.get("ProgramFiles", r"C:\Program Files")).joinpath("AnsysEM"), r"Win64\ansysedt.exe"),
+        (Path(os.environ.get("ProgramFiles", r"C:\Program Files")).joinpath("ANSYS Inc"), r"AnsysEM\ansysedt.exe"),
         (Path("/opt/AnsysEM"), "Linux64/ansysedt"),  # LINX64 Ansys 2024/R2 or older
         (Path("/ansys_inc"), "AnsysEM/ansysedt"),  # LINX64 Ansys 2025/R1 or newer
     ]
